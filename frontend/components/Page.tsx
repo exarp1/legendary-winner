a4e216ab8e6d9f903a432f0acbb4e574f2a07b14
exarp1 <exarp1@gmail.com>
2021-07-14 18:43:58 -0700

    07.14.2021: update styles

--- frontend/components/Page.tsx
+++ frontend/components/Page.tsx
@@ -1,16 +1,65 @@
 import { FC } from 'react';
+import Header from './Header'
+import styled, { createGlobalStyle } from 'styled-components'
+
+
+
+const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'radnika_next';
+  src: ('/static/radnikanext-medium-webfont.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+html {
+  --red: #ff0000;
+  --black: #393939;
+  --grey: #3a3a3a;
+  --gray: var(--grey);
+  --lightGrey: #e1e1e1;
+  --lightGray: var(--lightGray);
+  --ofWhite: #ededed;
+  --maxWidth: 1000px;
+  --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+body {
+  font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  padding: 0;
+  margin: 0;
+  font-size: 1.5rem;
+  line-height: 2;
+}
+a {
+  text-decoration: none;
+  color: var(---black);
+}
+a:hover {
+  text-decoration: underline;
+}
+button {
+  font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`
 
 interface PageProps {
   cool?: string;
-  // optional children of type reactNode
-  // children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-const Page: FC<PageProps>  = ({ children, cool }) => (
+const InnerStyles = styled.div`
+max-width: var(--maxWidth);
+margin: 0 auto;
+padding: 2rem;
+`;
+const Page: FC<PageProps>  = ({ children }) => (
   <div>
-    <h2>I am the page component</h2>
-    {children}
-    <h3>{cool}</h3>
+    <GlobalStyles />
+    <Header />
+    <InnerStyles> {children} </InnerStyles>
   </div>
 );
 
