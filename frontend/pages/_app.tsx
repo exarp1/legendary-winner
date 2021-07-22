82815186ba030c007c2b363d3e0cab4ab358bd1a
exarp1 <exarp1@gmail.com>
2021-07-21 18:45:17 -0700

    July21-backend
    * install wesbos pkg to make eslint / Prettier play nice together
    * update prettier config
    * configure database for basic operation

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,17 @@
-import { AppProps } from 'next/app'
-import Page from '../components/Page'
+import NProgress from "nprogress";
+import { AppProps } from "next/app";
+import Page from "../components/Page";
+import Router from "next/router";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Page>
       <Component {...pageProps} />
     </Page>
-  )
+  );
 }
